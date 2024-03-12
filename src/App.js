7e5fae51fe4cca9befbd6c1ecf5a1a27407ab9ef
HansakaM <mhansaka1@gmail.com>
2024-03-12 19:32:11 +0530

    create a navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <>
+    <Navbar/>
+  
+    </>
   );
 }
 
